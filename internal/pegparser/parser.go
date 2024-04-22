deabeae5f80ccbe9b1f610af40fc68692e00d5a5
jackson <jacksonwearn@gmail.com>
2024-04-22 18:48:35 -0400

    very rough parser built using pigeon

---
+++ internal/pegparser/parser.go
@@ -0,0 +1,18 @@
+package pegparser
+
+import (
+	"errors"
+	"strings"
+)
+
+func ParseInput(input string) (string, error) {
+	result, err := Parse("", []byte(input))
+	if err != nil {
+		return "", nil
+	}
+
+	if md, ok := result.(Markdown); ok {
+		return strings.TrimSpace(strings.Join(md.Lines, "\n\n")), nil
+	}
+	return "", errors.New("failed to parse cast result as Markdown")
+}
